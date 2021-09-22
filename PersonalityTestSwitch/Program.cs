5290655baf8cfef4df757af2858034d626d66767
rauno valling <rvalling@tktk.ee>
2021-09-22 18:42:13 +0300

    add new projects

---
+++ PersonalityTestSwitch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PersonalityTestSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisetada tema lemmikvärv;
+            // kui kasutaja sisestab "punane", konsool kuvab "oled romantiline";
+            // kui kasutaja sisestab "sinine", konsool kuvab "oled töökas";
+            // kui kasutaja sisestab "roheline", konsool kuvab "oled looduse sõber";
+            // kui kasutaja sisestab midagi muud, konsool kuvab "oled {userColor} ükssarvik";
+
+            Console.WriteLine("Sisesta oma lemmikvärv:");
+            string userColor = Console.ReadLine().ToLower();
+
+            switch (userColor)
+            {
+                case "punane": // if(userColor == "punane") 
+                    Console.WriteLine("Oled romantiline");
+                    break;
+                case "sinine":
+                    Console.WriteLine("Olen töökas.");
+                    break;
+                case "roheline":
+                    Console.WriteLine("Olen looduse sõber.");
+                    break;
+                default:
+                    Console.WriteLine($"Oled {userColor} üksarvik");
+                    break;
+            }
+
+            Console.WriteLine("Kena päeva!");
+
+
+        }
+    }
+}
